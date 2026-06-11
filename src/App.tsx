7d0d5961b440195f638338ffdd2130c3e654f55a
HaL-Fujita <xxmade.in.moonxx@gmail.com>
2026-06-11 21:43:28 +0900

    feat: App.tsx を追加

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <p>ここにプロフィールやコンポーネントを追加できます。</p>
+    </div>
+  );
+}
+
+export default App;
